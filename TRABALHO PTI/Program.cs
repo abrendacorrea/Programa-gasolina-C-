23e6647c0583344207d72da04ee85c8df1a4a9eb
abrendacorrea <brendacorrea.c2@gmail.com>
2023-04-30 12:23:34 -0300

    Adicionar arquivos de projeto.

---
+++ TRABALHO PTI/Program.cs
@@ -0,0 +1,51 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+
+      
+        int opcao;
+        do {
+
+            {
+                {
+                    Console.WriteLine(" Insira aqui o valor do alcool ");
+                    double alcool = double.Parse(Console.ReadLine());
+                    Console.WriteLine(" Insira aqui o valor da gasolina ");
+                    double gasolina = int.Parse(Console.ReadLine());
+
+                    double resultado = alcool / gasolina;
+                    Console.WriteLine(resultado = alcool / gasolina);
+
+                    if (resultado > 0.7)
+                    {
+                        Console.WriteLine("é mais vantajoso você abastecer com Gasolina!");
+                    }
+
+                    else if (resultado < 0.7)
+                    {
+                        Console.WriteLine("é mais vantajoso você abastecer com Alcool!");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Abasteça com Gasolina ou Alcool!");
+
+                    }
+
+
+                    Console.WriteLine("tecle 1 para realizar um novo calculo");
+                    Console.WriteLine("tecle 0 para sair");
+                }
+            }
+            opcao = int.Parse(Console.ReadLine());
+        } 
+        while (opcao != 0);
+        
+}
+}
+            
+
+
+
+
